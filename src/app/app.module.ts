8c172b0db777080fc761f19973723ea1635c4ad6
MohamedOussama1 <moe.oussama.saidi@gmail.com>
2023-09-19 23:06:02 +0100

    Added grid layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {CommonModule, NgOptimizedImage} from "@angular/common";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListProductComponent } from './list-product/list-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    NgOptimizedImage,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/ProductItem.ts
@@ -0,0 +1,11 @@
+export interface ProductItem{
+  id : number;
+  name : string;
+  price : number;
+  score: number;
+  description : string;
+  imgUrl : string;
+  isAvailable: boolean;
+}
+
+
